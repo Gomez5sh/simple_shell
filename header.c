ca9b78dd7fe5358b58b0d322d10ae1bfef53fc4a
g5sh <eagomez2387@gmail.com>
2019-11-20 15:04:51 +0000

    [ADD]: Add the header

---
+++ header.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <errno.h>
+#define PROMPT "#Cisfun> "
+#define ERROR_MS "shell: No such file or directory\n"
+
+char *_getenv(const char *name);
+
+#endif
