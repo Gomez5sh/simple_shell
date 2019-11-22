f809fe8794e86dc44fe6e73c4f33d9e5484dacbf
g5sh <eagomez2387@gmail.com>
2019-11-22 20:29:14 +0000

    [ADD]: Path proto

--- env_path.c
+++ env_path.c
@@ -28,3 +28,51 @@ char *_getenv(const char *name)
 	}
 	return (NULL);
 }
+
+
+
+char *command(char *order)
+{
+	etern
+	DIR *dir_env;
+	struct dirent *entry;
+	char *arch, *arch2;
+	int n;
+	char **change, **move, *envi;
+
+	change = strdup(*order);
+	move = strdup(*order);
+
+
+	envi = _getenv("PATH");
+
+	n = 0;
+
+	while (n != NULL)
+	{
+		*change = strtok(*move, ":=");
+
+		dir_env = opendir(*change);
+
+		if(dir_env == NULL)
+		{
+			perror("Error to read directory.");
+			return(1);
+		}
+
+		while((entry=readdir(dir_env)))
+		{
+			arch = entry->d_name;
+
+			arch2 = strcmpdir(arch, order);
+
+			if (arch == 0)
+				return (*change);
+		}
+		n++;
+		change++;
+		move++;
+	}
+
+	return("Not Found");
+}
--- header.h
+++ header.h
@@ -1,5 +1,7 @@
 #ifndef _HEADER_H_
 #define _HEADER_H_
+#ifndef _GNU_SOURCE_
+#define _GNU_SOURCE_
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,6 +11,7 @@
 #include <sys/types.h>
 #include <errno.h>
 #include <signal.h>
+#include <dirent.h>
 #define PROMPT "#Cisfun> "
 #define ERROR_MS "shell: No such file or directory\n"
 
@@ -18,5 +21,6 @@ int _strlen(char *s);
 void INThandler(int);
 char *_getenv(const char *name);
 int _strcmp(char *s1, char *s2);
+
 
 #endif
