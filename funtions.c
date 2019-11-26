d292e085d5c06c323aa68b5f66706dda9fd1bcac
cristian0497 <cristian04.97@hotmail.com>
2019-11-26 16:29:26 -0500

    [FEATURE] _strlen(); string_rec_exit(); free_buf(); INThanler(); _strcat(); Betty OK. valgrind OK

--- funtions.c
+++ funtions.c
@@ -15,7 +15,8 @@ int _strlen(char *s)
 	return (contchar);
 }
 /**
- * string_rec - funtion recognize the string arg when ir a call system funtion - 2
+ * string_rec_exit - funtion recognize the string
+ * arg when ir a call system funtion - 2
  * @s: string arg
  * Return: (0)none (1)exit (2)env
  */
@@ -27,12 +28,12 @@ int string_rec_exit(char *s)
 	len = 0;
 	if (!s)
 		return (-1);
-	for(cont = 0; s[cont] != '\0'; cont++)
+	for (cont = 0; s[cont] != '\0'; cont++)
 	{
-		if(s[cont] == _exit[cont])
+		if (s[cont] == _exit[cont])
 			len++;
-		if(len > 3)
-			return(1);
+		if (len > 3)
+			return (1);
 	}
 	return (0);
 }
@@ -42,11 +43,13 @@ int string_rec_exit(char *s)
  * @s: message to print before close a proces
  * @len: len of the s
  * @bufo: Addres of bufo
+ * @state: code of exit funtion
  * Return: void
  */
 void free_buf(char *s, int len, char *bufo, char *state)
 {
 	int state_ = 0;
+
 	if (s)
 		write(STDOUT_FILENO, s, len);
 	if (!state)
@@ -77,20 +80,20 @@ void INThandler(int n)
  */
 char *_strcat(char *dest, char *src)
 {
-    int n, x, len_dest, len_src;
-    char *ret;
+	int n, x, len_dest, len_src;
+	char *ret;
 
-    len_dest = _strlen(dest);
-    len_src = _strlen(src);
-    ret = malloc(sizeof(char) * (len_dest + len_src) + 1);
-    for (n = 0; dest[n] != '\0'; n++)
-    {
-      ret[n] = dest[n];
-    }
-    for (x = 0 ; src[x] != '\0'; x++, n++)
-    {
-      ret[n] = src[x];
-    }
-    ret[n] = '\0';
-    return (ret);
+	len_dest = _strlen(dest);
+	len_src = _strlen(src);
+	ret = malloc(sizeof(char) * (len_dest + len_src) + 1);
+	for (n = 0; dest[n] != '\0'; n++)
+	{
+		ret[n] = dest[n];
+	}
+	for (x = 0 ; src[x] != '\0'; x++, n++)
+	{
+		ret[n] = src[x];
+	}
+	ret[n] = '\0';
+	return (ret);
 }
