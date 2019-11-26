66601a1633843b7ec971c7b97a2abd66206eae29
cristian0497 <cristian04.97@hotmail.com>
2019-11-26 16:49:57 -0500

    [FEATURE] _strcmp(); findpath(); string_rec_path(); _atoi(); change_dir();

--- strcmp.c
+++ strcmp.c
@@ -1,8 +1,9 @@
 #include "header.h"
-/** 1
- * Compare if ths command have path or full path
- * @s: command line
- * return full path create to execve
+/**
+ * _strcmp - Compare if ths command have path or full path - 1
+ * @tmp: command line
+ * @str: string to compare
+ * Return: full path create to execve
  */
 int _strcmp(char *tmp, char *str)
 {
@@ -17,44 +18,46 @@ int _strcmp(char *tmp, char *str)
 	}
 	return (0);
 }
-/** 2
- *
- *
- *
+/**
+ * findpath - search the specific variable environment - 2
+ * @env: all enviremnt array key-value
+ * @to_find: key to search
+ * Return: string value
  */
 char *findpath(char **env, char *to_find)
 {
 	int cont;
 	char *ret_path = NULL;
 
-	for (cont = 0; env[cont] != NULL; cont ++)
+	for (cont = 0; env[cont] != NULL; cont++)
 	{
 		ret_path = strtok(env[cont], "=");
 		if (_strcmp(ret_path, (to_find + 1)) == 1)
 		{
 			ret_path = strtok(NULL, "\n");
-			return(ret_path);
+			return (ret_path);
 		}
 	}
 	write(STDOUT_FILENO, ERROR_PATH, 18);
 	return (NULL);
 }
-/** 3
- * str_
- *
- *
+/**
+ * string_rec_path - funtion check if string have or not the path - 3
+ * @tmp: string of getline
+ * Return: string with path
  */
 char *string_rec_path(char *tmp)
 {
 	char path[] = "/bin/";
 
 	if (tmp[0] == '/')
-		return(tmp);
-	return(_strcat(path, tmp));
+		return (tmp);
+	return (_strcat(path, tmp));
 }
-/** 4
- *
- *
+/**
+ * _atoi - converti strings in INT - 4
+ * @s: string to convert
+ * Return: int number
  */
 int _atoi(char *s)
 {
@@ -82,10 +85,11 @@ int _atoi(char *s)
 	return (number);
 }
 
-/** 4
- *
- *
- *
+/**
+ * change_dir - change the path pwd
+ * @newdir: new directory to change
+ * @bufo: free child proces to reset shell with other env path
+ * Retur: void
  */
 void change_dir(char *newdir, char *bufo)
 {
