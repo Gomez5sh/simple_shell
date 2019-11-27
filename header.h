959149ad62dc68dca1036187de0d2fd2047cd286
cristian0497 <cristian04.97@hotmail.com>
2019-11-26 23:03:10 -0500

    add, const envitonment

--- header.h
+++ header.h
@@ -11,8 +11,11 @@
 #include <signal.h>
 
 #define PROMPT "SHELL "
-#define ERROR_MS "shell: No such file or directory\n"
+#define ERROR_MS ": not found\n"
 #define ERROR_PATH "Fail to find PATH\n"
+
+extern char **environ;
+
 int _atoi(char *s);
 void free_buf(char *s, int len, char *bufo, char *arg);
 int string_rec_exit(char *s);
